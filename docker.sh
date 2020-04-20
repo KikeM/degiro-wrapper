58f324d2196c384fa24ddc4307d1c6710e74d083
Maximiliano Greco <mgreco@ets.es>
2020-04-20 08:14:52 +0200

    ENH: Fix docker script

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-docker build -t degiro:0.1.0 .
-docker run --name degiro \
-           --volume $(pwd):/root/degiro-wrapper \
+docker build -t degiro:0.1.0 . \
+&& docker run --name degiro \
+           --volume $(pwd):$(pwd) \
             -it degiro:0.1.0
