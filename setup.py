d69b105ae71ba7084b94961b168065278103be7f
Maximiliano Greco <mgreco@ets.es>
2020-04-20 08:10:12 +0200

    BLD: Complete dependencies in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="degiro-wrapper",
-    install_requires=[],
+    install_requires=[
+        "QuantStats",
+        "tqdm",
+        "xlrd",
+        "pandas",
+        "numpy",
+    ],
     packages=find_packages('src'),
     package_dir={'': 'src'},
 )
